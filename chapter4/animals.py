b6c886d6aae7df4594a17e771ecf08bdcaead509
Ibrahima KANE <kaneelhadji2610@yahoo.fr>
2021-01-31 08:51:35 +0100

    4.1

---
+++ chapter4/animals.py
@@ -0,0 +1,7 @@
+animals = ["dog", "parrot", "tortle"]
+
+for animal in animals:
+    print("A " + animal + " would make a great pet")
+
+print("\n")
+print("Any of this animals would make a great pet!")
---
+++ chapter4/pizzas.py
@@ -0,0 +1,5 @@
+pizzas = ["Orientale","chiken barbecue", "Spicy Hot One"]
+for pizza in pizzas:
+    print("I like " + pizza + " pizza")
+
+print("\nI really love pizzas!")
