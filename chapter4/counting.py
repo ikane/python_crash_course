0e6d846300df111aa052ee4e4da10a90344bd4c6
ikane <kaneelhadji2610@yahoo.fr>
2021-02-06 17:16:28 +0100

    4.7

--- chapter4/counting.py
+++ chapter4/counting.py
@@ -8,3 +8,11 @@ million = list(range(1,1000000))
 print("Min: " + str(min(million)))
 print("Max: " + str(max(million)))
 print("Sum: " + str(sum(million)))
+
+print("======== Odd numbers ===========")
+for odd in range(1,20, 2):
+    print(odd)
+
+print("======== Multiple of three using List compensation ===========")
+multiples = [3 * value for value in range(3,30)]
+print(multiples)
