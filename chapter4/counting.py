fc59f36d083a7d0056dea86dec066dd427e82468
ikane <kaneelhadji2610@yahoo.fr>
2021-02-06 14:24:27 +0100

    4.2

---
+++ chapter4/counting.py
@@ -0,0 +1,10 @@
+for value in range(1,11):
+    print(value)
+
+print("\n$$$$$$$$$$$$$\n")
+
+million = list(range(1,1000000))
+
+print("Min: " + str(min(million)))
+print("Max: " + str(max(million)))
+print("Sum: " + str(sum(million)))
