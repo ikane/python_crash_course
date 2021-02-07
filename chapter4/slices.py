3498665b9c261b41ad493a2f2630a2211bf868f6
ikane <kaneelhadji2610@yahoo.fr>
2021-02-07 18:36:58 +0100

    4.11

--- chapter4/slices.py
+++ chapter4/slices.py
@@ -12,3 +12,13 @@ print(animals[middle:end])
 print("The last three items in the list are:")
 
 print(animals[-3:])
+
+print("*************************************************")
+
+pizzas = ["Orientale","chiken barbecue", "Spicy Hot One"]
+friend_pizzas = pizzas[:]
+pizzas.append("Basilica Poulet")
+friend_pizzas.append("Urban Kebab")
+
+print("My favorite pizzas are :" + str(pizzas))
+print("My frend's favorite pizzas are :" + str(friend_pizzas))
